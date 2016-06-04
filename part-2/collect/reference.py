3cdcd135ad3a0faebc8bc40757e4744db29079bd
Kuma Li <li.jun.kuma@gmail.com>
2016-06-05 00:56:06 +0800

    fix: finish is and == difference

--- part-2/collect/reference.py
+++ part-2/collect/reference.py
@@ -2,15 +2,25 @@
 # encoding: utf-8
 
 print '''
-number, string, tuple 不可变数据属于值传递
-dict, list, set 等可变对象类型 引用传递
-'''
-
-'''
 is 如果两个变量指向同一个 object, 返回 True
 == 如果两个变量的值相同，返回 True
 '''
+a = b = 1
+print 'a = b = 1; a == b', a == b
+print 'a is b', a is b
 
+a = b = [1]
+print 'a = b =[1], a == b', a == b
+print 'a is b', a is b
+c = [1]
+print 'c = [1]; c == b', c == b
+print 'c is b', c is b
+
+
+print '''
+number, string, tuple 不可变数据属于值传递
+dict, list, set 等可变对象类型 引用传递
+'''
 
 list_a = [0,1,3,4,5]
 list_b = list_a
